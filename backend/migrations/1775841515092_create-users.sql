65b22fa5c6df03b858e238b0bfd2a5c67df71897
jayDevariya2027 <jaydevariya2027@gmail.com>
2026-04-10 22:59:35 +0530

    chore: backend setup with migrations

---
+++ backend/migrations/1775841515092_create-users.sql
@@ -0,0 +1,15 @@
+-- Up Migration
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_users_email ON users(email);
+
+-- Down Migration
+-- DROP TABLE users;
